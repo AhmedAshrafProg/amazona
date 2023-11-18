6905d3f2f2d11f1d193a35841df5747e1c4ebbf8
AhmedAshrafProg <ahmashweb@gmail.com>
2023-11-18 16:18:41 +0200

    beginning_react

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/"> amazona </a>
+      </header>
+      <main>
+        <section>product 1</section>
+        <section>product 2</section>
+        <section>product 3</section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
